514f634898c6350f0f4f76b4cce51c244e4cd0fb
qnddjKJH <qnddjkjh.dev@gmail.com>
2023-05-02 15:25:18 +0900

    add restaurats API

--- server/routes/api/restaurants/restaurants.js
+++ server/routes/api/restaurants/restaurants.js
@@ -1,25 +1,9 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-    fastify.get('/', async function (request, reply) {
-        reply.send([
-            {
-              "name": "용다방",
-              "menu": [
-                {
-                  "name": "애플 시나몬 에이드",
-                  "price": 6500,
-                  "duration": 5
-                },
-                {
-                  "name": "카페모카",
-                  "price": 6000,
-                  "duration": 5
-                }
-              ],
-              "address": "서울시 마포구 양화로 1111",
-              "rating": 4.5
-            }
-          ])
-    })
-  }
+  fastify.get('/', async function (request, reply) {
+      const restaurants = await this.mongo.db.collection('restaurants').find({}).toArray()
+
+      reply.send(restaurants)
+  })
+}
