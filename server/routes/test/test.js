f7cb3a545a416c55889409f9d8ce7a8ea07b129b
qnddjKJH <qnddjkjh.dev@gmail.com>
2023-05-02 15:26:11 +0900

    delete test routes

--- server/routes/test/test.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    const test_data = this.mongo.db.collection('test').find({}).toArray();
-    return test_data
-  })
-}
